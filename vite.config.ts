59621998d80197b87c61887531846a339997b95a
nguyenyou <tunt081295@gmail.com>
2025-05-26 10:20:00 +0700

    🚢 ship it

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: process.env.PAGES_BASE_PATH || '/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
